4b865b8ff776b82e659ad9c4b5bda6a05aa5b3ca
Miguel Araya <miaraya@gmail.com>
2017-03-26 11:48:11 +0700

    base Views

---
+++ application/views/teamView.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+	<head>
+		<!-- Meta
+		============================================= -->
+		<?php $this->load->view('metaView'); ?>
+
+		<title><?php echo $page_title;?></title>
+	</head>
+
+	<body class="stretched">
+		<div id="fb-root"></div>
+			<script>(function(d, s, id) {
+			  var js, fjs = d.getElementsByTagName(s)[0];
+			  if (d.getElementById(id)) return;
+			  js = d.createElement(s); js.id = id;
+			  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.8";
+			  fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));
+			</script>
+
+		<!-- Document Wrapper
+		============================================= -->
+		<div id="wrapper" class="clearfix">
+
+			<!-- Header
+			============================================= -->
+			<?php $this->load->view('headerView'); ?>
+			<!-- #header end -->
+
+			<!-- Content
+			============================================= -->
+			
+			
+
+			<!-- #content end -->
+
+			<!-- Footer
+			============================================= -->
+			<?php $this->load->view('footerView'); ?>
+			<!-- #footer end -->
+		</div> <!-- wrapper end -->
+
+		<div id="gotoTop" class="icon-angle-up"></div>
+
+		<?php $this->load->view('scriptView'); ?>
+
+	</body>
+</html>
+
