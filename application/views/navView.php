2c7e66c98658fdc2bf5fc770f4950f48754ceb50
Miguel Araya <miaraya@gmail.com>
2017-08-16 10:14:43 +0700

    Pricing Menu

--- application/views/navView.php
+++ application/views/navView.php
@@ -5,17 +5,17 @@
 		<li><a href="<? echo base_url(); ?>">HOME</a></li>
 		<li><a href="<?php echo site_url('training'); ?>">TRAINING</a>
 			<ul>
-				<li><a href="<?php echo site_url('training'); ?>">Disciplines</a></li>
+				<li><a href="<?php echo site_url('training'); ?>">DISCIPLINES</a></li>
 				<li><a href="<?php echo site_url('team'); ?>">TEAM</a></li>
 				<li><a href="<?php echo site_url('pricing'); ?>">PRICING</a></li>
-				<li><a href="<?php echo site_url('time-table'); ?>">Time Table</a></li>
+				<li><a href="<?php echo site_url('time-table'); ?>">TIME TABLE</a></li>
 
 
 
 			</ul>
 		</li>
 
-		<!-- <li><a href="<?php echo site_url('sponsorship'); ?>">SPONSORSHIP</a></li> -->
+		<li><a href="<?php echo site_url('pricing'); ?>">PRICING</a></li> -->
 		<li><a href="<?php echo site_url('gallery'); ?>">GALLERY</a></li>
 		<li><a href="<?php echo site_url('faqs'); ?>">FAQS</a></li>
 		<li><a href="<?php echo site_url('testimonials'); ?>">TESTIMONIALS</a></li>
