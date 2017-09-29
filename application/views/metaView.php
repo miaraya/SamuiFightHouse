e17657ec3e42f3073f895391596cad5f2875e85a
Miguel Araya <miaraya@gmail.com>
2017-09-29 11:19:58 +0700

    Open Graph

--- application/views/metaView.php
+++ application/views/metaView.php
@@ -3,6 +3,13 @@
 <meta name="google-site-verification" content="3d5SGivLUJ8xlaqeHfFXXr9DAQO_FV983Zi48mb9d_I" />
 <meta name="description" content="Muay Thai in Koh Samui. Training camp and personal training in Koh Samui, Thailand" />
 <meta name="keywords" content="Muay Thai in Koh Samui, Thailand, training camp, personal training, fitness training" />
+<meta property="og:image" content="http://www.samuifighthouse.com/images/facebook-ad.png" />
+<meta property="og:url" content="http://www.samuifighthouse.com" />
+<meta property="og:title" content="Muay Thai in Koh Samui - Samui Fight House" />
+
+
+
+
 
 
 
